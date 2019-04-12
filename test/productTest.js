03d4dccb5166cef326e2e4cee049fcc263088920
Charles BEYER <charles.beyer.pro@gmail.com>
2019-04-12 22:37:16 +0200

    Update productTest.js

--- test/productTest.js
+++ test/productTest.js
@@ -22,16 +22,8 @@ it("Should have the Game item in the store", async() => {
     assert.equal(item[1], "MegaMan", "The item should have the correct name.");
     assert.equal(item[2], "Made by Capcom", "The item should have the correct description.");
     // item[3].toNumber() now throws an error due to precision errors.
-    assert.equal(web3.fromWei(item[3].toString(),'ether'), 1, "The item should have ID number 1.");
-    assert.equal(item[4].toNumber(), 1, "The item should have ID number 1.");
-    assert.equal(item[5].toNumber(), 1, "The item should have ID number 1.");
-    assert.equal(item[6].toNumber(), 1, "The item should have ID number 1.");
+    assert.equal(web3.fromWei(item[3].toString(),'ether'), 1, "The item should have the correct price.");
+    assert.isFalse(item[4], "The item should not be sold.");
+    assert.isFalse(item[5], "The item should not be sent.");
+    assert.isFalse(item[6], "The item should not be received.");
 });
-
-item[id].id,
-item[id].itemName,
-item[id].itemDescription,
-item[id].itemPrice,
-item[id].sold,
-item[id].sent,
-item[id].received
