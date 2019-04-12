a9803ed865da8030c29dc47c41caf1690a6113a0
Charles BEYER <charles.beyer.pro@gmail.com>
2019-04-12 17:06:49 +0200

    Added verification steps

--- contracts/Product.sol
+++ contracts/Product.sol
@@ -17,6 +17,8 @@ contract Product {
     Item[] public items;
 
     function newItem(string name, string description, uint price) public returns(uint) {
+        require(price > 0, "Price must be greater than 0.");
+        uint checkingLength = items.length;
         items.push(
             Item({
                 buyer: address(0),
@@ -30,6 +32,7 @@ contract Product {
                 received: false           
             })
         );
+        assert(checkingLength + 1 == items.length);
         return items.length + 1;
     }
 
@@ -48,19 +51,26 @@ contract Product {
     }
 
     function buyItem(uint id) public payable{
+        require(item[id].itemPrice == msg.value)
         item[id].buyer = msg.sender;
         item[id].sold = true;
     }
 
     function receivedItem(uint id) public{
+        require(item[id].sold == true);
+        require(item[id].sent == true);
+        require(item[id].buyer == msg.sender);
         item[id].received = true;
     }
 
     function sentItem(uint id) public{
+        require(item[id].sold == true);
         item[id].sent = true;
     }
     
     function claimFunds(uint id) public{
+        require(item[id].seller == msg.sender);
+        require(item[id].received == true);
         msg.sender.transfer(item[id].itemPrice);
     }
 
