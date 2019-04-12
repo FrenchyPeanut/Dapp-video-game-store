2d27f6278bc462270b4384432027f8bce899ec2b
Charles BEYER <charles.beyer.pro@gmail.com>
2019-04-12 15:43:52 +0200

    Adding set and get functions

--- contracts/Product.sol
+++ contracts/Product.sol
@@ -6,9 +6,9 @@ contract Product {
         address buyer;
         address seller;
         uint id;
-        string name;
-        string description;
-        uint price;
+        string itemName;
+        string itemDescription;
+        uint itemPrice;
         bool sold;
         bool sent;
         bool received;
@@ -16,8 +16,33 @@ contract Product {
 
     Item[] public items;
 
-    function doStuff(string name, string description, uint price) public returns(uint) {
-        
+    function newItem(string name, string description, uint price) public returns(uint) {
+        items.push(
+            Item({
+                buyer: address(0),
+                seller: msg.sender,
+                id: items.length + 1,
+                itemName: name,
+                itemDescription: description,
+                itemPrice: price,
+                sold: false,
+                sent: false,
+                received: false           
+            })
+        );
+        return items.length + 1;
+    }
+
+    function getItem(uint id) public view returns(){
+        return( 
+            item[id].id,
+            item[id].itemName,
+            item[id].itemDescription,
+            item[id].itemPrice,
+            item[id].sold,
+            item[id].sent,
+            item[id].received
+            );
     }
 
  
